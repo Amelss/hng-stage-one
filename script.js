1fba9de0844ac452190e7f3eafa8405a61a49292
Amelss <ameleyka@gmail.com>
2023-09-07 13:01:07 +0100

    Updating day date to be weekday names

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const todaysDate = document.getElementById("currentDate")
 const todaysTime = document.getElementById('currentTime')
 
 const getDate = new Date()
-const day = getDate.getDate()
 const month = getDate.getMonth()
 const year = getDate.getFullYear()
-
+const day = new Date().toLocaleString("en-GB", { weekday: "long" });
 const hours = getDate.getHours()
 const minutes = getDate.getMinutes()
 
 todaysDate.innerHTML = `${day}/ ${month}/${year}`
 todaysTime.innerHTML = `${hours}:${minutes}`
+
+
 
