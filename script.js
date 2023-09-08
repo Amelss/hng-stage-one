2da0fbe882811ca98717a2efac93081cd4c3b34b
Amelss <ameleyka@gmail.com>
2023-09-08 11:41:35 +0100

    Refactoring js code

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const todaysDate = document.getElementById("currentDate")
 const todaysTime = document.getElementById('currentTime')
 
 const getDate = new Date()
-const month = getDate.getMonth()
-const year = getDate.getFullYear()
 const day = new Date().toLocaleString("en-GB", { weekday: "long" });
 const time = getDate.toUTCString()
 
